eab08f61d8cb7a9a41d504363db144d786ccce71
RiskiYuliawati <RiskiYuliawati01@gmail.com>
2019-02-14 10:02:39 -0800

    Create contoh membuat aplikasi nota.cpp
    tugas c++ nota

---
+++ contoh membuat aplikasi nota.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+int main ()
+{
+	double No, tgl, noseri, telp;
+	string Tanggal, Nama, name, KL, JL, PS;
+	
+	
+	cout<<"			IT SERVIC								"<<endl;
+	cout<<"		 ACP SMKN 4 PADALARANG							"<<endl;
+	cout<<"		jl. Raya Padalarang 451							"<<endl;
+cout<<"============================================================"<<endl;
+	
+	
+	cout<<"No : "; cin>>No;						cout<<"jenis Laptop : "; cin>>JL;
+	cout<<"Tanggal : "; cin>>tgl>>Tanggal;		cout<<"No Seri : "; cin>>noseri;		
+	cout<<"Nama : "; cin>>Nama>>name;			cout<<"telp : "; cin>>telp;
+	
+	
+	cout<<"Form Keluhan  "<<endl;
+	cout<<"Keluhan Laptop : "; cin>>KL; 
+	cout<<"Perkiraan Selesai : "; cin>>tgl>>PS; 
+	
+	
+system ("PAUSE");	
+return 0;
+}
+
